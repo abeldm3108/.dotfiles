4dfd6c48489504f2108e6d9d7af0d78cc5729976
abel <abeldm3108@gmail.com>
2022-04-04 13:33:09 +0200

    change bspwm layout

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,11 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-current=$(xrandr --listactivemonitors | grep -v Monitors | grep -Eo '[^ ]+$')
-if [ $current == "DP-1" ]
-then
-	polybar -q main_external -c "$HOME/.dotfiles/polybar/config.ini" &
-else
-	polybar -q main -c "$HOME/.dotfiles/polybar/config.ini" &
-fi
+polybar -q main -c "$HOME/.dotfiles/polybar/config.ini" &
+# current=$(xrandr --listactivemonitors | grep -v Monitors | grep -Eo '[^ ]+$')
+# if [ $current == "DP-1" ]
+# then
+# 	polybar -q main_external -c "$HOME/.dotfiles/polybar/config.ini" &
+# else
+# 	polybar -q main -c "$HOME/.dotfiles/polybar/config.ini" &
+# fi
 
